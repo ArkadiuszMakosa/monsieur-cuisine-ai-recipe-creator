a097763d8e337a19715cf0c41b8751ff29f4dde0
Arkadiusz Mąkosa <aos@spyro-soft.com>
2026-02-27 08:46:18 +0100

    Add initial comit

---
+++ monsieur-cuisine-ai-recipe-creator/monsieur-cuisine-ai-recipe.Domain.Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Based on imput reciepie, output step by step instruction for monsieur cuisine
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
